c8dbd1c6388295564d2d615e7f8a8cef49d744eb
Jiddan Najwan <mjnjgaming@gmail.com>
2026-09-22 19:54:23 +0700

    inisialisasi aplikasi kriptografi

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+# from klasik import 
+# from modern import 
+
+st.set_page_config(page_title="Kriptografi", layout="wide")
+
+st.title("Kriptografi")
+
+tab_algoritma_klasik, tab_algoritma_modern, tab_super_enkripsi = st.tabs(
+    ["Algoritma Klasik", "Algoritma Modern", "Super Enkripsi"])
+
+with tab_algoritma_klasik:
+    pass
+
+with tab_algoritma_modern:
+    pass
+
+with tab_super_enkripsi:
+    pass
